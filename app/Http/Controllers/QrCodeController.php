d898ac9042f8eb250b0e8440a7bd312798603659
hatimbelghiti <belghitihatim00@gmail.com>
2025-03-05 13:49:34 +0000

    solving some bugs

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\URL;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QrCodeController extends Controller
@@ -19,6 +19,9 @@ class QrCodeController extends Controller
 
     public function generate(User $user)
     {
+            $hashids = new Hashids('your-secret-salt', 10);
+        $hashedId = $hashids->encode($user->id);
+        $url = URL::signedRoute('invite' , ['user' => $hashedId], now()->addMinutes(30));
         $qrCode = QrCode::size(300)->generate(route('qrcode.addFriend', ['user' => $user->id]));
         return view('qrcode', compact('qrCode'));
     }
--- routes/web.php
+++ routes/web.php
@@ -16,49 +16,45 @@ Route::get('/', function () {
 });
 
 Route::get('/registre' ,[redrecteController::class ,'index']);
-Route::get('/connection',[redrecteController::class,'connection'])->name('login');
+Route::get('/connection',[redrecteController::class,'connection']);
+Route::get('/dashboard',[profileController::class,'dashboard']);
+Route::get('/reset',[redrecteController::class,'reset']);
+Route::get('/Suggestions',[FreindController::class,'Suggestions']);
+Route::get('/index',[postController::class,'getAllPosts']);
+Route::get('/logout', function () {
+    Auth::logout();
+    return redirect('/');
+})->name('logout');
+Route::get('/amis',[FreindController::class,'mesAmies']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [profileController::class, 'dashboard']);
-    Route::get('/reset', [redrecteController::class, 'reset']);
-    Route::get('/Suggestions', [FreindController::class, 'Suggestions']);
-    Route::get('/index', [postController::class, 'getAllPosts']);
-    Route::get('/logout', function () {
-        Auth::logout();
-        return redirect('/');
-    })->name('logout');
-    Route::get('/amis', [FreindController::class, 'mesAmies']);
+Route::get('/invitations',[FreindController::class,'showRequestEnvoye']);
 
-    Route::get('/invitations', [FreindController::class, 'showRequestEnvoye']);
+Route::get('/profile/{id}',[profileController::class,'ToProfile'])->name('consulteProfile');
 
-    Route::get('/profile/{id}', [profileController::class, 'ToProfile'])->name('consulteProfile');
+Route::post('/registreForm',[AuthController::class,'store'])->name('registreForm');
+Route::post('/connectionForm',[AuthController::class,'connection'])->name('connectionForm');
+Route::post('/uploadImage',[profileController::class,'upload'])->name('uploadImage');
+Route::post('/addPost',[postController::class,'store'])->name('poste.store');
+Route::post('/addFreind',[FreindController::class,'addFreind'])->name('addFreind');
 
-    Route::post('/registreForm', [AuthController::class, 'store'])->name('registreForm');
-    Route::post('/connectionForm', [AuthController::class, 'connection'])->name('connectionForm');
-    Route::post('/uploadImage', [profileController::class, 'upload'])->name('uploadImage');
-    Route::post('/addPost', [postController::class, 'store'])->name('poste.store');
-    Route::post('/addFreind', [FreindController::class, 'addFreind'])->name('addFreind');
-
-    Route::delete('/posts/{id}', [postController::class, 'destroy'])->name('posts.destroy');
-    Route::put('/posts/{id}', [postController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{id}', [postController::class, 'destroy'])->name('posts.destroy');
+Route::put('/posts/{id}', [postController::class, 'update'])->name('posts.update');
 
 
 //
 
 
-    Route::get('/forgot-password', [redrecteController::class, 'showForgotPasswordForm'])->name('forgot.password');
-    Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
+Route::get('/forgot-password', [redrecteController::class, 'showForgotPasswordForm'])->name('forgot.password');
+Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
 // Route::get('/reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('password.reset');
-    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
 
 
-    Route::post('/accepter', [FreindController::class, 'AccepterFreind'])->name('AccepterInvitation');
-    Route::post('/refuser', [FreindController::class, 'RefuserFreind'])->name('RefuserFreind');
+Route::post('/accepter',[FreindController::class,'AccepterFreind'])->name('AccepterInvitation');
+Route::post('/refuser',[FreindController::class,'RefuserFreind'])->name('RefuserFreind');
 
 
-    Route::post('/addcomment', [CommentController::class, 'store'])->name('addComment');
+Route::post('/addcomment',[CommentController::class,'store'])->name('addComment');
 
-    Route::post('/addLike', [LikeController::class, 'store'])->name('addLike');
-    Route::get('/generate-qrcode', [QrCodeController::class, 'generate']);
-    Route::get('/qr-code', [FreindController::class, 'addFriendQr']);
-});
+Route::post('/addLike',[LikeController::class,'store'])->name('addLike');
+Route::get('/generate-qrcode', [QrCodeController::class, 'generate']);
