2d346a912a989e210e8e517204863aebf04f1d46
sara boulahia <saraboulahia32@gmail.com>
2025-03-06 11:25:04 +0000

    update views add facebook button

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -52,13 +52,17 @@
                             Mot de passe oublié ?
                         </a>
                     </div>
-                    <div class="row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <br/>
-                            <a href="{{ route('redirect.google') }}">
-                            <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" alt="Google Sign-In Button">
-                        </a>
-
+                    <div class="flex flex-col items-center space-y-4">
+                        <div class=" w-64">
+                            <a title="Login with Google" href="{{ route('redirect.google') }}">
+                                <img class="w-full" src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" alt="Google Sign-In Button">
+                            </a>
+                        </div>
+                        <div class="w-64">
+                            <a title="Login with Facebook" href="{{ route('redirect.facebook') }}" class="btn-facebook flex items-center justify-center border border-gray-300 rounded-lg p-2 w-full bg-blue-600 text-white">
+                                <img class="h-6 w-6 mr-2" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook Logo">
+                                Sign in with Facebook
+                            </a>
                         </div>
                     </div>
 
